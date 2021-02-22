aea723b371fe96b2a610d47fc148ae0311ed0862
geniltonmarcos <genilton.marcos@gmail.com>
2021-02-22 12:07:03 -0300

    Update Config.cs

--- POCAzureServiceBus/Config.cs
+++ POCAzureServiceBus/Config.cs
@@ -2,7 +2,7 @@
 {
     static class Config
     {
-        public static string ConnectionString = "Endpoint=sb://gmpti-service-bus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=tnBY/gpEqyORPlnPngnukWfUc6bwvnUlHpBrgVY7m3I=";
+        public static string ConnectionString = "Endpoint=sb://";
         public static string QueueName = "fila_01";
     }
 }
